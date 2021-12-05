5540d273df89bec0bea2bea13731a20ab0c7dfbf
Ryan Reed <git@ryanreed.net>
2021-12-04 19:18:08 -0500

    Cleaning up template

--- puzzles/TEMPLATE_test_dayXX.py
+++ puzzles/TEMPLATE_test_dayXX.py
@@ -11,12 +11,7 @@ INPUTS_DIR = f"{pathlib.Path(__file__).parent.parent}/inputs"
 
 @pytest.fixture
 def example_data1():
-    input_path = f"{INPUTS_DIR}/dayXX-example1.txt"
-    return aoc.parse(pathlib.Path(input_path).read_text().strip())
-
-@pytest.fixture
-def example_data2():
-    input_path = f"{INPUTS_DIR}/dayXX-example2.txt"
+    input_path = f"{INPUTS_DIR}/dayXX-example.txt"
     return aoc.parse(pathlib.Path(input_path).read_text().strip())
 
 
@@ -27,13 +22,13 @@ def dayXX_data():
 
 
 @pytest.mark.skip(reason="Not implemented")
-def test_example1(example_data1):
-    assert aoc.part1(example_data1) == ...
+def test_example1(example_data):
+    assert aoc.part1(example_data) == ...
 
 
 @pytest.mark.skip(reason="Not implemented")
-def test_example2(example_data2):
-    assert aoc.part2(example_data2) == ...
+def test_example2(example_data):
+    assert aoc.part2(example_data) == ...
 
 
 @pytest.mark.skip(reason="Not implemented")
